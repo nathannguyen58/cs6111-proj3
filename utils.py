dacdd01a834bb736da28cb5691ded7d88958696c
Nathan <nn2505@columbia.edu>
2022-04-16 17:21:22 -0400

    implemented apriori algorithm

---
+++ utils.py
@@ -0,0 +1,82 @@
+from collections import defaultdict
+from itertools import combinations
+
+itemList = []
+
+data = [['1', ['pen', 'ink', 'diary']], ['2', ['pen', 'ink', 'diary']], ['3', ['pen', 'diary']], ['4', ['pen', 'ink', 'soap']]]
+
+
+def apiori_frequent_set_mining(data, min_sup, min_conf):
+    
+    c = defaultdict(int)
+    l = defaultdict(int)
+    large_itemset = defaultdict(int)
+    k = 1
+    for id, items in data:
+        for item in items:
+            c[item] += 1
+            if (item not in itemList):
+                itemList.append(item)
+
+    sorted(itemList)
+    min_sup = (min_sup * len(data))
+
+    for item, count in c.items():
+        if count >= min_sup:
+            l[frozenset([item])] = count
+            large_itemset[frozenset([item])] = count/len(data) * 100
+
+    while l and k <= len(itemList):        #while l is not empty and our item set size hasn't reached the length of the original item set
+        k += 1
+        prev_sets = list(l.keys())
+        new_c = set()
+        c = defaultdict(int)
+        ##join step
+
+        for i in range(0, len(prev_sets)):   #iterate through all the itemsets in k - 1
+            for j in range(i+1, len(prev_sets)): #same thing
+                u = prev_sets[i].union(prev_sets[j])
+                if (len(u) == k):            #add to new candidate itemset if length = k
+                    new_c.add(u)
+        
+        # prune step
+        for itemSet in new_c:
+            subsets = list(combinations(list(itemSet), k-1))
+            prunned = False
+            for subset in subsets:
+                if subset and frozenset(subset) not in l:
+                    prunned = True
+                    break
+            if not prunned:
+                c[frozenset(itemSet)] = 0
+
+        
+        # get frequency of each new set in c in data 
+        for itemSet in c:
+            for id, items in data:
+                if(itemSet.issubset(items)):
+                    c[itemSet] += 1
+        
+        # get the new l
+        for item, count in c.items():
+            if count >= min_sup:
+                l[item] = count
+                large_itemset[item] = count/len(data) * 100
+        
+    for item, count in large_itemset.items():
+        print(item, count)
+                
+
+apiori_frequent_set_mining(data, 0.7, 0)
+
+
+        
+            
+        
+
+
+
+
+
+    
+    
